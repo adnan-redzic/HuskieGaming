a318b76b1b1616bd6aa87ddbb1a9e40e79d3a174
adnan-redzic <111529475+adnan-redzic@users.noreply.github.com>
2023-04-06 10:58:37 -0500

    Animations
    Slay has an idle animation that faces left which is also used when he is move left. But I have to make it so that when the D key is pressed, that he is move right and facing towards right. Same thing applies to Saquon but not in the game yet.

---
+++ RB_Saquan/Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    // reference to the Animator component
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the animator component of the Game Object
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // change the animator's AnimState variable if a key is pressed
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetInteger("AnimState", 1);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetInteger("AnimState", 1);
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetInteger("AnimState", 1);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetInteger("AnimState", 1);
+        }
+
+        else
+        {
+            animator.SetInteger("AnimState", 0);
+        }
+    }
+}
