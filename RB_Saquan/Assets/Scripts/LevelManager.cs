2a7570b785c982e56c8d88555800c0288cf3fb5e
adnan-redzic <111529475+adnan-redzic@users.noreply.github.com>
2023-05-04 10:46:04 -0500

    5/4/23
    New Update

---
+++ RB_Saquan/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string level)
+    {
+        Debug.Log("Attempting to load " + level);
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Attempting to quit...");
+        Application.Quit();
+    }
+}
