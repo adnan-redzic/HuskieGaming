efcefbba9dc21ec2c0dbaa87e3da3f6cf636e6fb
adnan-redzic <111529475+adnan-redzic@users.noreply.github.com>
2023-05-12 10:28:11 -0500

    5/12/23
    New Update.

---
+++ RB_Saquan/Assets/Scripts/GoalArea.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalArea : MonoBehaviour
+{
+    public Text MyText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyText.text = "" + score;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag ("Pickable"))
+        {
+            score = score + 1;
+        }
+
+        if (coll.CompareTag ("Pickable"))
+        {
+            coll.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ RB_Saquan/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private Text score;
+    private int scoreAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreAmount = 0;
+        score = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        score.text = scoreAmount.ToString();
+    }
+
+    public void AddScre()
+    {
+        scoreAmount += 1;
+    }
+}
